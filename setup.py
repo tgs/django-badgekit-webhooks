f135c4bf7bb1e38e1c60a6325b57096b0a5fe41d
Thomas Grenfell Smith <thomathom@gmail.com>
2014-04-24 15:58:09 -0400

    Add forgotten dependency on PyJWT

--- setup.py
+++ setup.py
@@ -23,6 +23,9 @@ setup(
     tests_require=[
         "Django>=1.4",
     ],
+    install_requires=[
+        "pyjwt",
+    ],
     test_suite="runtests.runtests",
     classifiers=[
         "Development Status :: 4 - Beta",
