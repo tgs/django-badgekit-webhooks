e7d1e7dd1bd4061a085ed552062e39d2694efdcc
Thomas Grenfell Smith <thomathom@gmail.com>
2014-04-28 15:50:33 -0400

    Prevent XSS in claim page; fix on Django 1.4 and 3.x

--- badgekit_webhooks/tests/claim_tests.py
+++ badgekit_webhooks/tests/claim_tests.py
@@ -11,6 +11,24 @@ from badgekit_webhooks import views
 
 class ClaimPageTest(TestCase):
     def testCanGetClaimPage(self):
-        url = views.create_claim_url('http://example.com/assertion.json')
+        url = views.create_claim_url(b'http://example.com/assertion.json')
         resp = self.client.get(url)
         self.assertEqual(resp.status_code, 200)
+
+    def testQuotesDoNotAppear(self):
+        url = views.create_claim_url(b"http://evil.com/quote'quote")
+        resp = self.client.get(url)
+        self.assertFalse(b"quote'quote" in resp.content)
+        self.assertFalse(b"quote&#39;quote" in resp.content)
+
+    def testBracketsDoNotAppear(self):
+        url = views.create_claim_url(b"http://evil.com/angle<angle>angle")
+        resp = self.client.get(url)
+        self.assertFalse(b"angle<" in resp.content)
+        self.assertFalse(b"angle>" in resp.content)
+
+    def testMoreChars(self):
+        url = views.create_claim_url(b"http://evil.com/semi;dquote\"")
+        resp = self.client.get(url)
+        self.assertFalse(b'semi;' in resp.content)
+        self.assertFalse(b'dquote"' in resp.content)
