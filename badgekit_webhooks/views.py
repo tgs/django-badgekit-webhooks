cbdc41e4fd8658a6bf94939a622bd2661bdef940
Thomas Grenfell Smith <thomathom@gmail.com>
2014-05-01 15:31:28 -0400

    Nose captures logs during testing, so these are less annoying now

--- badgekit_webhooks/views.py
+++ badgekit_webhooks/views.py
@@ -57,11 +57,12 @@ def badge_issued_hook(request):
             body_sig = payload['body']['hash']
             # Assuming sha256 for now.
             if body_sig != hashlib.sha256(request.body).hexdigest():
-                # Timing attack shouldn't matter: attacker can see the sig anyway.
+                # TODO test this
+                logging.warning("Bad JWT signature on webhook body")
                 return HttpResponse('Bad body signature', status=403)
 
         except (jwt.DecodeError, KeyError):
-            #logging.exception('Bad JWT auth')
+            logging.exception('Bad JWT auth')
             return HttpResponse('Bad JWT auth', status=403)
 
     try:
