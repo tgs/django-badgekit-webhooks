52d167db64a6fa7c87bfd37c097e15d2d6c79715
Thomas Grenfell Smith <thomathom@gmail.com>
2014-05-01 13:43:11 -0400

    Add utils module, get some code out of the views

--- badgekit_webhooks/views.py
+++ badgekit_webhooks/views.py
@@ -17,31 +17,7 @@ import jwt
 import hashlib
 import logging
 from django.core.urlresolvers import reverse
-import base64
-
-
-# From Django 1.6 - not present in 1.4.  django.utils.http.urlsafe_...
-def urlsafe_base64_encode(s):
-    """
-    Encodes a bytestring in base64 for use in URLs, stripping any trailing
-    equal signs.
-    """
-    return base64.urlsafe_b64encode(s).rstrip(b'\n=')
-
-def urlsafe_base64_decode(s):
-    """
-    Decodes a base64 encoded string, adding back any trailing equal signs that
-    might have been stripped.
-    """
-    s = s.encode('utf-8') # base64encode should only return ASCII.
-    try:
-        return base64.urlsafe_b64decode(s.ljust(len(s) + len(s) % 4, b'='))
-    except (LookupError, BinasciiError) as e:
-        raise ValueError(e)
-
-
-decode_param = urlsafe_base64_decode
-encode_param = urlsafe_base64_encode
+from .utils import decode_param, encode_param
 
 
 logger = logging.getLogger(__name__)
