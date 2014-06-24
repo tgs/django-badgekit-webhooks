4874f1b1a7a3ff465493f601f5f056bf8f3f1921
Thomas Grenfell Smith <thomathom@gmail.com>
2014-06-24 14:08:48 -0400

    Move badge instance list beneath 'badges' url

--- badgekit_webhooks/urls.py
+++ badgekit_webhooks/urls.py
@@ -9,8 +9,6 @@ urlpatterns = patterns(
     url(r"^hello/$", "badgekit_webhooks.views.hello", name="badgekit_webhooks_hello"),
     url(r"^issued/$", "badgekit_webhooks.views.badge_issued_hook",
         name="badge_issued_hook"),
-    url(r"^instances/([-A-Za-z.0-9_]+)/$", staff_member_required(views.badge_instance_list),
-        name="badge_instance_list"),
     url(r"^claim/([-A-Za-z0-9_]+)/$", 'badgekit_webhooks.views.claim_page'),
     url(r"^claim/([-A-Za-z0-9_]+)/email/(html|text)$", 'badgekit_webhooks.views.show_claim_email',
         name="show_claim_email"),
@@ -19,4 +17,6 @@ urlpatterns = patterns(
     url(r"^claimcode/([-A-Za-z.0-9_]+)/$",
         views.ClaimCodeClaimView.as_view(), name='claimcode_claim'),
     url(r"^badges/$", "badgekit_webhooks.views.list_badges_view", name="badges_list"),
+    url(r"^badges/([-A-Za-z.0-9_]+)/instances/$", staff_member_required(views.badge_instance_list),
+        name="badge_instance_list"),
     )
