930335202aa57cc78422ed67603b5ca708005a61
Nate Otto <nate@ottonomy.net>
2014-06-30 10:03:09 -0700

    added tests for verifying assertion baseurl

--- badgekit_webhooks/tests/claim_tests.py
+++ badgekit_webhooks/tests/claim_tests.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 from django.test import TestCase
 from django.core.urlresolvers import reverse
+from django.conf import settings
 import json
 import jwt
 import hashlib
@@ -9,6 +10,8 @@ import httpretty
 import re
 
 from badgekit_webhooks import views
+
+settings.BADGEKIT_API_URL = "http://example.com"
 
 badge_info_dummy = {
         'badge': 'http://example.com/badge',
@@ -19,11 +22,12 @@ badge_info_dummy = {
 
 def register_dummy():
     httpretty.register_uri(httpretty.GET,
-            re.compile(r'evil.com/.*'),
-            body=json.dumps(badge_info_dummy))
-    httpretty.register_uri(httpretty.GET,
             re.compile(r'example.com/.*'),
             body=json.dumps(badge_info_dummy))
+    httpretty.register_uri(httpretty.GET,
+            re.compile(r'wrongissuer.com/.*'),
+            body=json.dumps(badge_info_dummy))
+
 
 
 class ClaimPageTest(TestCase):
@@ -37,14 +41,14 @@ class ClaimPageTest(TestCase):
     @httpretty.activate
     def testQuotesDoNotAppear(self):
         register_dummy()
-        url = views.create_claim_url(b'http://evil.com/quote"quote')
+        url = views.create_claim_url(b'http://example.com/quote"quote')
         resp = self.client.get(url)
         self.assertFalse(b'quote"quote' in resp.content)
 
     @httpretty.activate
     def testBracketsDoNotAppear(self):
         register_dummy()
-        url = views.create_claim_url(b"http://evil.com/angle<angle>angle")
+        url = views.create_claim_url(b"http://example.com/angle<angle>angle")
         resp = self.client.get(url)
         self.assertFalse(b"angle<" in resp.content)
         self.assertFalse(b"angle>" in resp.content)
@@ -52,7 +56,7 @@ class ClaimPageTest(TestCase):
     @httpretty.activate
     def testMoreChars(self):
         register_dummy()
-        url = views.create_claim_url(b"http://evil.com/semi;dquote\"")
+        url = views.create_claim_url(b"http://example.com/semi;dquote\"")
         resp = self.client.get(url)
         self.assertFalse(b'dquote"' in resp.content)
 
@@ -60,10 +64,10 @@ class ClaimPageTest(TestCase):
 class AssertionVerifyTests(TestCase):
     # Test that it rejects evil.com
     @httpretty.activate
-    def testSomething(self):
+    def cantGetWrongClaimURL(self):
         with self.settings(BADGEKIT_API_URL="http://example.com/",
                 BADGEKIT_VERIFY_ASSERTION_URL=True):
             resp = self.client.get(views.create_claim_url(b"http://evil.com/angle"))
-
-    # Test that it accepts example.com
+            self.assertEqual(resp.status_code, 400)
+    # We tested that it accepts example.com above with testCanGetClaimPage
 
